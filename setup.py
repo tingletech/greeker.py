d3f33e73170d2ad4e37f66032955b0ada9f14899
tingletech <brian.tingle.cdlib.org@gmail.com>
2013-02-26 22:41:21 -0800

    adding more required libraries

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='greeker',
       author="Brian Tingle",
       author_email="brian.tingle.cdlib.org@gmail.com",
       url="http://tingletech.github.com/greeker.py/",
-      install_requires=["inflect>=0.2.1", "lxml>=2.3.2", "nltk>=2.0.1rc2-git"],
+      install_requires=["inflect>=0.2.1", "lxml>=2.3.2", "nltk>=2.0.1rc2-git", "numpy", "argparse"],
       py_modules=['greeker'],
       scripts=['greeker.py'],
       )
